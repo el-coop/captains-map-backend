47b96215dcc15011ce30494b4b33cfce368ceb2d
Nur Bar <LCD344@yahoo.com>
2019-07-12 22:56:40 +0300

    added getCsrf route

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
 
+router.get('/getCsrf',(req, res) => {
+	return res.send({});
+});
+
 router.use('/', require('./media'));
 router.use('/auth', require('./auth'));
 router.use('/marker', require('./marker'));
@@ -8,5 +12,7 @@ router.use('/geocode', require('./geocode'));
 
 router.use('/search', require('./search'));
 router.use('/crawler', require('./crawler'));
+
+
 
 module.exports = router;
---
+++ tests/integration/getCsrf.spec.js
@@ -0,0 +1,11 @@
+import test from 'ava';
+import request from "supertest";
+import app from "../../app";
+
+
+test.serial('It returns an empty response with a csrf header', async t => {
+	const response = await request(app).get('/api/getCsrf');
+
+	t.is(response.status, 200);
+	t.deepEqual(response.body, {});
+});
